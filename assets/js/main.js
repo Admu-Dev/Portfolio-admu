3444d211909f21134b21aa3c2752ef5b9ab0dc82
Admu-Dev <adrien.muzeaux@gmail.com>
2023-01-13 19:00:45 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/*
+__________________________________________________________________________
+
+                    Menu
+__________________________________________________________________________
+ */
+
+const toggleBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+const navbar = document.getElementById("navbar");
+const body = document.body;
+const linksText = document.querySelectorAll(".links .item a p");
+
+toggleBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (!toggleBtn.className.includes("active")) {
+    toggleBtn.classList.add("active");
+    links.classList.add("active");
+    navbar.classList.add("active");
+  } else {
+    toggleBtn.classList.remove("active");
+    links.classList.remove("active");
+    navbar.classList.remove("active");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (body.offsetWidth <= 850) {
+    linksText.forEach((link) => (link.style = "display: block;"));
+  }
+  if (window.scrollY >= 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
+
+/*
+__________________________________________________________________________
+
+                Animated SVG Scrolling Mouse Icon
+__________________________________________________________________________
+ */ 
+
+jQuery(document).ready(function($){
+	
+	$('a.scroll-link').click(function(e){
+		e.preventDefault();
+		$id = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $($id).offset().top -20
+		}, 200);
+	});
+	
+});
+
+/*
+__________________________________________________________________________
+
+                Slider
+__________________________________________________________________________
+ */ 
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
---
+++ forms/contact.php
@@ -0,0 +1,53 @@
+<?php
+/* Récupération des informations du formulaire*/
+  $name = stripslashes(trim($_POST['name']));
+  $email = stripslashes(trim($_POST['mail']));
+  $subject = stripslashes(trim($_POST['subject']));
+  $message = stripslashes(trim($_POST['message']));
+
+  /*Vérifie si l'adresse mail est au bon format */
+  $regex_mail = "#[a-z0-9]{1,}[\-\_\.a-z0-9]{0,}@[a-z]{2,}[\-\_\.a-z0-9]{0,}\.[a-z]{2,6}$#";
+
+  /*Vérifie qu il n y est pas d erreur dans adresse mail*/
+  if (!preg_match($regex_mail, $_POST['mail'])){
+    echo $email;
+    $courriel = 0; 
+  }else{
+    $courriel = 1;
+  }   
+
+  if ($courriel == 1){
+    /*Envoi du mail*/
+
+    /*Le destinataire*/
+    $to="adrien.muzeaux@gmail.com";
+
+    /*Le sujet du message qui apparaitra*/
+    $sujet="Message depuis le site";
+    $msg = '';
+    /*Le message en lui même*/
+    /*$msg = 'Mail envoye depuis le site' "rnrn";*/
+    $msg .= 'Nom : '.$name."rnrn";
+    $msg .= 'Mail : '.$email."rnrn";
+    $msg .= 'Motif : '.$subject."rnrn";
+    $msg .= 'Message : '.$message."rnrn";
+    /*Les en-têtes du mail*/
+    $headers = 'From: MESSAGE DU SITE FAFA<adrien.muzeaux@gmail.com>'."rn";
+    $headers .= "rn";
+    /*L'envoi du mail - Et page de redirection*/
+    mail($to, $sujet, $msg, $headers);
+    header('Location: ../index.html#contact');
+    echo '<script type="text/javascript">' . 
+    'document.getElementById("sent-message").innerHTML = "Your message has been sent. Thank you!";
+    
+    document.getElementById("sent-message").classList.add("alert-success")' .
+    '</script>';
+  }else{
+    header('Location: ../index.html#contact');
+    echo '<script type="text/javascript">' . 
+    'document.getElementById("error-message").innerHTML = "Il y a eu un erreur lors de l\'envoie du mail. Si cela perciste, merci de m\'envoyer un mail directement sans passer par le site web";
+    
+    document.getElementById("error-message").classList.add("alert-danger")' .
+    '</script>';
+  }
+?>
